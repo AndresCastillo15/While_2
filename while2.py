cc4af9ced8afe6fe6bf688bc5b8c02a06bb730ad
AndresCastillo15 <andrescastillo@colegioguanenta.edu.co>
2025-02-14 06:48:35 -0500

    actualizacion

--- while2.py
+++ while2.py
@@ -1,1 +1,23 @@
-#while_2
+#While_2
+
+#input 
+
+print("╔═══════════════════════════╗")
+H = int(input("Digite su Numero: "))
+print("╚═══════════════════════════╝")
+
+#proccesing
+
+Q = H/5
+N = 0
+
+while H>=Q:
+    H = H - 0.1 * H
+    N = N + 1
+
+
+# Output
+
+print("╔═════════════════════════════════════════════════════════════════╗")
+print("Los rebotes desde el valor digitado en total son: "+str(N))
+print("╚═════════════════════════════════════════════════════════════════╝")
